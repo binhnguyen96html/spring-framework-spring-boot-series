1bff4bba9051c85cd181d6e42832a76555bb6c52
Binh <binhnguyendxbp@gmail.com>
2024-09-19 06:41:45 -0700

    init project and test api /test

---
+++ src/main/java/com/example/api/NewAPI.java
@@ -0,0 +1,18 @@
+package com.example.api;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class NewAPI {
+
+	@GetMapping("/test")
+
+	public String testAPI() {
+
+		return "success";
+
+	}
+
+}
